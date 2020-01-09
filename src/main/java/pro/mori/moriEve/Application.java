342472c7aab98e3a7aaa3626a4f08d2472896776
末日 <1302796097@qq.com>
2020-01-09 14:15:30 +0800

    ok
    v1.0

---
+++ src/main/java/pro/mori/moriEve/Application.java
@@ -0,0 +1,12 @@
+package pro.mori.moriEve;
+
+import pro.mori.eveCore.doom;
+
+public class Application {
+    public static void main(String[] args) {
+        new doom(Application.class,args)
+        .test()
+        .run();
+
+    }
+}
