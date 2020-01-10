d834fc074d1647b69e2f34f157337e46f76da15d
末日 <1302796097@qq.com>
2020-01-10 14:50:12 +0800

    v1.0.2

--- src/main/java/pro/mori/moriEve/Application.java
+++ src/main/java/pro/mori/moriEve/Application.java
@@ -5,8 +5,13 @@ import pro.mori.eveCore.doom;
 public class Application {
     public static void main(String[] args) {
         new doom(Application.class,args)
-        .test()
-        .run();
+                .test()
+                .test_Class()
+                .test_Url()
+                .test_Input()
+                .test_UrlEcoding()
+                .test_UrlPost()
+                .run();
 
 
     }
